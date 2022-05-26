7fc6487cd02d98c546569c218c3d8af7b5a3202a
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-05-26 15:34:18 -0300

    Initial commit

---
+++ Assets/Scripts/contarSegundos.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class contarSegundos : MonoBehaviour
+{
+    public float timeElapsed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeElapsed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            timeElapsed = 0;
+        }
+        Debug.Log(timeElapsed);
+
+        // float segundos = Mathf.Floor(Time.time);
+        // Debug.Log(segundos);
+    }
+}
