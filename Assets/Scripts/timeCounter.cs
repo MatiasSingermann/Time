45d25dbdea23789547350def64a95935767d0536
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2022-05-26 16:01:45 -0300

    Clase terminada

---
+++ Assets/Scripts/timeCounter.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timeCounter : MonoBehaviour
+{
+    public Text txtTimeCounter;
+    float elapsedTime;
+    public GameObject myCube;
+    float tiempo;
+    float nextSpawnTime;
+    float freqInSeconds;
+
+    bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = Time.deltaTime;
+        elapsedTime = 0;
+        isPaused = false;
+        nextSpawnTime = 2;
+        freqInSeconds = Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused == false)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+
+        txtTimeCounter.text = Mathf.Floor(elapsedTime).ToString();
+
+        if(nextSpawnTime < Time.time)
+        {
+            Instantiate(myCube);
+            nextSpawnTime += freqInSeconds;
+        }
+    }
+}
